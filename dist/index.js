103a546671d19de223e43d804a9c237278ae6b8c
Thomas Bui <thomasthienbui@gmail.com>
2020-12-11 10:56:42 +0700

    Integrated lodash and mocha

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var App_1 = require("./App");
+var lodash_1 = __importDefault(require("lodash"));
 var out = "the sum of 5 and 5 is " + App_1.add(5, 5);
 console.log(out);
+var arr = [1, 2, 3];
+lodash_1.default.fill(arr, 10);
+var i = 0;
+for (i = 0; i < 3; i++) {
+    console.log(arr[i]);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import { add } from "./App";
-
+import ld from "lodash";
 
 const out: string = "the sum of 5 and 5 is " + add(5, 5);
 
 console.log(out);
+
+
+let arr: number[] = [ 1, 2 ,3];
+
+ld.fill(arr, 10);
+
+let i: number = 0;
+
+for(i = 0; i < 3; i++) {
+  console.log(arr[i]);
+}
 
